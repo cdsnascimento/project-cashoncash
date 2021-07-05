7c3231c51c2dafd7f607b8c63ff6320d8f587341
cdsnascimento <cdsnascimento@hotmail.com>
2021-07-05 16:36:47 -0300

    Poject Created

---
+++ backend/src/main/java/com/dominos/cashoncash/CashoncashApplication.java
@@ -0,0 +1,13 @@
+package com.dominos.cashoncash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashoncashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashoncashApplication.class, args);
+	}
+
+}
